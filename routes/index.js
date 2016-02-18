02a59f24fda79cf90397e24ae53e653bc6e14a55
chaopan <chaopan0329@gmail.com>
2016-02-18 05:43:27 +0000

    added useless code for lab

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,19 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
+ 
+ 
+ 
 exports.view = function(req, res){
-    projects["grid"] = false;
-    console.log(projects["grid"]);
-  	res.render('index', projects);
+var random_num = Math.random();
+console.log(random_num);
+
+if (random_num >= 0) {
+  projects['grid'] = false;
+  res.render('index', projects);
+} else {
+  res.redirect('/grid');
+}
 };
 
 exports.viewGrid = function(req, res){
